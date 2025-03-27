4f2da2e5beccf9631adadff9ef061088642b0071
liyuqing <14766422+liyuqingcs@user.noreply.gitee.com>
2025-03-27 19:35:48 +0800

    pawnmanager pawninteractcontroll testpawn -- lyq

---
+++ Assets/Scripts/Pawn/TestPawnSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+public class TestPawnSpawner : MonoBehaviour
+{
+    void Update(){
+        // 检查 P 键是否按下
+        if (Input.GetKeyDown(KeyCode.P)){
+            Debug.Log("P 键被按下");
+
+            // 生成一个随机位置的 Pawn
+            Vector2 randomPos = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
+
+            // 调用 PawnManager 的 CreatePawn 方法
+            if (PawnManager.Instance != null){
+                PawnManager.Instance.CreatePawn(randomPos);
+                Debug.Log($"在位置 {randomPos} 创建 Pawn");
+            }
+            else{
+                Debug.LogError(" PawnManager.Instance 为空，请检查 PawnManager 绑定！");
+            }
+        }
+    }
+}
