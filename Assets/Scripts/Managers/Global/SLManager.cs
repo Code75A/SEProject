c42f9037f8570c96ee1ca54389d70a67fe51bfd9
david200404 <130828406+david200404@users.noreply.github.com>
2025-04-10 18:31:28 +0800

    Update SLManager.cs
    lyl update SLManager-4.10

--- Assets/Scripts/Managers/Global/SLManager.cs
+++ Assets/Scripts/Managers/Global/SLManager.cs
@@ -2,38 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SLManager : MonoBehaviour
-{
+public class SLManager : MonoBehaviour{
     public static SLManager Instance;
-    class gameData
-    {
-        //TODO: 填入需要存储的数据
+
+    [System.Serializable]
+    class gameData{
+        public PawnManager pawnManager; // 存储 PawnManager 的数据
     }
 
-    void Awake()
-    {
-        if (Instance == null)
-        {
+    void Awake(){
+        if (Instance == null){
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else
-        {
+        else{
             Destroy(gameObject);
         }
     }
 
     public void Save(){
         gameData data = new gameData();
-        string json = JsonUtility.ToJson(data);
-        PlayerPrefs.SetString("SaveData", json);
+        data.pawnManager = PawnManager.Instance; // 将 PawnManager 的数据存入 gameData
+
+        string json = JsonUtility.ToJson(data, true); // 序列化为 JSON
+        PlayerPrefs.SetString("SaveData", json); // 存储到 PlayerPrefs
+        Debug.Log("保存成功: " + json);
     }
+
     public void Load(){
         string json = PlayerPrefs.GetString("SaveData");
-        gameData data = JsonUtility.FromJson<gameData>(json);
-        if (data == null)
-        {
+        if (string.IsNullOrEmpty(json)){
+            Debug.LogWarning("没有找到保存的数据！");
             return;
         }
+
+        gameData data = JsonUtility.FromJson<gameData>(json); // 反序列化 JSON
+        if (data == null || data.pawnManager == null){
+            Debug.LogWarning("加载失败，数据为空！");
+            return;
+        }
+
+        JsonUtility.FromJsonOverwrite(JsonUtility.ToJson(data.pawnManager), PawnManager.Instance); // 将数据覆盖到 PawnManager
+
+        // 重新实例化 Pawn 的 GameObject
+        foreach (var pawn in PawnManager.Instance.pawns){
+            pawn.Instance = GameObject.Instantiate(PawnManager.Instance.pawnPrefab, pawn.position, Quaternion.identity);
+            pawn.Instance.name = $"Pawn_{pawn.id}";
+            PawnInteractController controller = pawn.Instance.AddComponent<PawnInteractController>();
+            controller.pawn = pawn;
+        }
+
+        Debug.Log("加载成功: " + json);
     }
 }
