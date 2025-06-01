e810dcbe295a2bd6204a99cd6a29135e54cffc66
EstaRLemoen <221220155@smail.nju.edu.cn>
2025-06-02 02:08:33 +0800

    6.2 Update Event and Crop Factor Framework, finished but not test --dym

--- Assets/Scripts/Managers/Local/EventManager.cs
+++ Assets/Scripts/Managers/Local/EventManager.cs
@@ -30,28 +30,33 @@ public class EventManager : MonoBehaviour
         public int arrival;
         public int end;
         public int predictability_level;
-        public bool IsPredictable()
-        {
-            return false;
-        }
-        public void Arrive()
-        {
-            ;
-        }
-        public void End()
-        {
-            ;
-        }
-        public void DailyImpact()
-        {
-            ;
-        }
+        //public virtual bool IsPredictable(){ return false; }
+        public virtual void Arrive(){ ; }
+        public virtual void End(){ ; }
+        public virtual void DailyImpact(){ ; }
     }
-    public class Weather : Event { }
+    public class Weather : Event
+    {
+        public enum WeatherType
+        {
+            Sunny, Rainy, Snowy, Stormy, Total
+        }
+        public WeatherType weather_type;
+    }
     public class PestDisaster : Event
     {
         public int aim_crop;
-        public int control_level;
+        public int damage_rate;
+        public override void Arrive()
+        {
+            UIManager.Instance.DebugTextAdd("<<Event>> " + name + " has arrived.");
+            CropManager.Instance.SetCropPestDisaster(aim_crop, 1.0f - damage_rate);
+        }
+        public override void End()
+        {
+            UIManager.Instance.DebugTextAdd("<<Event>> " + name + " has ended.");
+            CropManager.Instance.RemoveCropPestDisaster(aim_crop);
+        }
     }
 
     List<Event> eventList = new List<Event>();
@@ -60,31 +65,29 @@ public class EventManager : MonoBehaviour
         int now_day = TimeManager.Instance.GetCurrentDay();
         foreach (var it in eventList)
         {
-            // Event not arrive
+            // Event end (last day)
+            if (now_day <= it.end){
+                // Events need to be Removed
+                if (now_day == it.end) it.End();
+                // Remove ended event
+                eventList.RemoveAll(s => now_day >= s.end);
+            }
+            // Events not arrive
             if (now_day < it.arrival) continue;
-            // Event arrive
+            // Events arrive
             if (now_day == it.arrival) it.Arrive();
             // during Event
-            if (now_day >= it.arrival && now_day < it.end)
-            {
+            if (now_day >= it.arrival && now_day < it.end){
                 it.DailyImpact();
-                // Event end (last day)
-                if (now_day == it.end - 1) it.End();
             }
-
         }
-        // Remove ended event
-        eventList.RemoveAll(s => now_day >= s.end);
     }
-    // Start is called before the first frame update
     void Start()
     {
         //TODO: SLManager应当初始化载入Event列表
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        return;
-    }
+    // void FixedUpdate()
+    // {
+    //     return;
+    // }
 }
